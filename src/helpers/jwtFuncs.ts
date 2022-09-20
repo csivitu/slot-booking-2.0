e68d4afc11835f6b28943e11afa0b93715ff825e
salt57 <sourishgupta02@gmail.com>
2022-09-21 00:56:49 +0530

    fix: error handling for jwt

--- src/helpers/jwtFuncs.ts
+++ src/helpers/jwtFuncs.ts
@@ -2,9 +2,13 @@ import { verify } from "jsonwebtoken";
 import { config } from "../config";
 
 export const verifyAccessToken = (token: string) => {
+  try {
     const payload = verify(token, config.jwt.secret);
     if (typeof payload === "string") {
       throw new Error("Invalid token");
     }
-    return payload as { name: string, email: string };
-  };
+    return payload as { name: string; email: string };
+  } catch (error) {
+    throw error;
+  }
+};
