e47b2769e651b17a78eb17656e77425405f668f3
salt57 <sourishgupta02@gmail.com>
2022-09-22 23:13:45 +0530

    fix: docker for node app

--- src/entities/slot.ts
+++ src/entities/slot.ts
@@ -2,10 +2,6 @@ import { prop, Ref } from "@typegoose/typegoose";
 import { User } from "./user";
 
 export class Slot {
-
-  @prop({ required: true })
-  public slotDate!: Date;
-
   @prop({ required: true })
   public startTime!: Date;
 
